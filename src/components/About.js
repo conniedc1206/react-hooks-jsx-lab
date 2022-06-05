05f3a5f08a7a01fc2dc193f10cd8797cf17e5a44
Connie Park <conniedc1206@gmail.com>
2022-06-05 10:10:40 -0700

    commit tree efaae94d48d16ca5aaf7ec7407d91feecfa6807b

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,18 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  //a <div> element with an id of "about"
+  //inside the div element, there should be three child elements:
+    //an <h2> element with the content of "About Me"
+    //a <p> element with any content of your choosing
+    //an <img> element with the src attribute set to the imported image variable, and the alt attribute set to "I made this"
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I am a software developer ready to learn!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 function NavBar() {
-  // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  //return a <nav> element
+  //inside nav element
+    //an <a> element with href attrubute set to #home and content text set to "Home"
+    //an <a> element with href attribute set to #about, and content text set to "About"
+  return (
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+    </nav>
+  );
 }
 
 export default NavBar;
